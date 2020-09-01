8771bc9bc43549b75a5d85719d66f33aeff047a8
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2020-09-01 19:56:50 +0200

    Revert "Add Project to Repository"
    This reverts commit 9ed119239eeac4510ad6f92923b923670f2a70e0.

--- BotTwitter/config.js
+++
@@ -1,9 +0,0 @@
-var twit = require('twit');
-var config = require('./config.js');
-
-module.exports = {
-    consumer_key: 'iIkr3mFMIuOLMT1uOfmetJtAx',
-    consumer_secret: 'rnnfL3Fs7QrSF1DCkP2h8vQJaz6G10uF9j7KwkcpcQEyN6Xe3q',
-    access_token: '1300531004533362695-IG73dc5Uywm1k2DMRcUv6UJZSLnqCF',
-    access_token_secret: 'zw29Aucjs6gy4dwGbnoPOXzS3lJmcPYU5N3D7fSMi5OCr'
-};
