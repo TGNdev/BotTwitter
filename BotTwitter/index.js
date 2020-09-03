f9069249ce3ad5364b7b9e7e51e0764b63c8592a
ThomasGrossmann <47849503+ThomasGrossmann@users.noreply.github.com>
2020-09-03 22:57:23 +0200

    WIP

--- BotTwitter/index.js
+++ BotTwitter/index.js
@@ -1,18 +1,15 @@
 var twit = require('twit/lib/twitter');
 var config = require('./config.js');
+var listMessages = require('./messages.js');
 
 var Twitter = new twit(config);
 
-var messages = ["Hello World ! Breathing is very important so don't forget to breathe !", "Hey ! I hope you didn't forget to breathe today !"];
-var messageLocation = 0;
+var messageLocation = Math.floor(Math.random() * listMessages.messages.count());
 var messageNumber = 1;
-
-var today = new Date();
-var time = today.getHours() + ":" + today.getMinutes();
 
 var writeTweet = function() {
     Twitter.post('statuses/update', {
-        status: "Reminder N°" + messageNumber + ' // At ' + time + "\n🫁 " + messages[messageLocation] + " 🌬️"
+        status: "Reminder !" + "\n🌬️ " + listMessages.messages[messageLocation] + " 🌬️"
     }, function(err, data, response) {
         console.log(data)
     });
---
+++ BotTwitter/messages.js
@@ -0,0 +1,12 @@
+let messages = [
+    "Hello World ! Breathing is very important so don't forget to breathe !",
+    "Hey ! I hope you didn't forget to breathe today !",
+    "Please bro, breathe...",
+    "Did you know that we're breathing like 23'000 times a day ? Let's increase, breathe !!!",
+    "Forgetting to breathe remains the only fatal distraction.",
+    "You know, the secret to longevity is to keep breathing ! *wink*",
+    "Morning is youthful, just by breathing, it makes us young again. Enjoy !",
+    "When we breathe a flower, we breathe its genitals. Hmm, who cares tho ?"
+];
+
+module.exports={messages};
